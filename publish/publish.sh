316f9e442039083fd3669a169d8a6a00fd1366eb
Alberto González <alberto.gonzalez@uji.es>
2020-06-08 17:11:29 +0200

    Add/update CI scripts

--- publish/publish.sh
+++ publish/publish.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if [ ! -z "$NPM_AUTH_TOKEN" ]; then
+    echo "Configuring NPM credentials..."
+    echo "registry=https://registry.npmjs.com/" >> ~/.npmrc
+    echo "//registry.npmjs.com/:_authToken=$NPM_AUTH_TOKEN" >> ~/.npmrc
+fi
+
 PACK_DIR=package;
 
 publish() {
