40adc1d6eb964e55269d976c3d2426901d0da1e0
Alberto González <alberto.gonzalez@uji.es>
2020-06-11 18:05:43 +0200

    Add callback manager and recognizers store mocks

---
+++ demo/app/tests/internal/activity-recognition/recognizers/index.ts
@@ -0,0 +1,36 @@
+import { RecognizerCallbackManager } from "nativescript-context-apis/internal/activity-recognition/recognizers/callback-manager";
+import { RecognitionCallback } from "nativescript-context-apis/internal/activity-recognition/recognizers";
+import { ActivityChange } from "nativescript-context-apis/internal/activity-recognition";
+import { RecognizerStateStore } from "nativescript-context-apis/internal/activity-recognition/recognizers/state/store";
+import { Resolution } from "nativescript-context-apis/internal/activity-recognition";
+
+export function createCallbackManagerMock(): RecognizerCallbackManager {
+    return {
+        add(callback: RecognitionCallback) {
+            return 0;
+        },
+        notifyAll(activityChange: ActivityChange) {
+            return null;
+        },
+        remove(callbackId: number) {
+            return null;
+        },
+        removeAll() {
+            return null;
+        },
+    } as RecognizerCallbackManager;
+}
+
+export function createRecognizersStateStoreMock(): RecognizerStateStore {
+    return {
+        isActive(recognizer: Resolution): Promise<boolean> {
+            return Promise.resolve(false);
+        },
+        markAsActive(recognizer: Resolution): Promise<void> {
+            return Promise.resolve();
+        },
+        markAsInactive(recognizer: Resolution): Promise<void> {
+            return Promise.resolve();
+        },
+    };
+}
