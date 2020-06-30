d561be94d63ccecc47503dd38ebd6ce7ac610de5
Alberto González <alberto.gonzalez@uji.es>
2020-06-30 17:09:16 +0200

    Add android medium recognizer manager

---
+++ demo/app/tests/internal/activity-recognition/recognizers/medium-res/android/manager.android.ts
@@ -0,0 +1,42 @@
+import { android as androidApp } from "tns-core-modules/application/application";
+import { AndroidMediumResRecognizerManager } from "nativescript-context-apis/internal/activity-recognition/recognizers/medium-res/android/manager.android";
+
+describe("Android medium res recognition manager", () => {
+    if (!androidApp) {
+        return;
+    }
+
+    const recognizerManager = new AndroidMediumResRecognizerManager();
+
+    it("allows to check if the recognition mechanism is ready", () => {
+        const isReady = recognizerManager.isReady();
+        expect(isReady === true || isReady === false).toBeTrue();
+    });
+
+    it("allows to prepare the recognition mechanism when not ready", () => {
+        const permissionPromise = recognizerManager.prepare();
+        expect(permissionPromise).toBeInstanceOf(Promise);
+    });
+
+    it("allows to start listening to native activity updates", async () => {
+        try {
+            await recognizerManager.startListening();
+        } catch (e) {
+            const isReady = recognizerManager.isReady();
+            expect(isReady).toBeFalse();
+        }
+    });
+
+    it("allows to stop listening to native activity updates", async () => {
+        try {
+            await recognizerManager.stopListening();
+        } catch (e) {
+            const isReady = recognizerManager.isReady();
+            expect(isReady).toBeFalse();
+        }
+    });
+
+    afterAll(async () => {
+        await recognizerManager.stopListening();
+    });
+});
---
+++ src/internal/activity-recognition/recognizers/medium-res/android/manager.android.ts
@@ -0,0 +1,31 @@
+import {
+  AndroidAbstractRecognizerManager,
+  StartOptions,
+} from "../../abstract-manager.android";
+
+import ActivityUpdateReceiver = es.uji.geotec.contextapis.activityrecognition.ActivityUpdateReceiver;
+import ActivityRecognition = com.google.android.gms.location.ActivityRecognition;
+
+export class AndroidMediumResRecognizerManager extends AndroidAbstractRecognizerManager {
+  constructor() {
+    super(ActivityUpdateReceiver.class);
+  }
+
+  protected handleStart(
+    context: android.content.Context,
+    intent: android.app.PendingIntent,
+    options: StartOptions
+  ): com.google.android.gms.tasks.Task<any> {
+    return ActivityRecognition.getClient(context).requestActivityUpdates(
+      options.detectionInterval ? options.detectionInterval : 0,
+      intent
+    );
+  }
+
+  protected handleStop(
+    context: android.content.Context,
+    intent: android.app.PendingIntent
+  ): com.google.android.gms.tasks.Task<any> {
+    return ActivityRecognition.getClient(context).removeActivityUpdates(intent);
+  }
+}
