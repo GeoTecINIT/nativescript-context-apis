c89e8f903207fff86c239d3db96d953da7480cc3
Miguel <al341802@uji.es>
2020-08-11 12:12:27 +0200

    Use promisify in tf-model setup tasks. Check if model was previously downloaded

--- src/internal/activity-recognition/recognizers/high-res/android/tf-model.android.ts
+++ src/internal/activity-recognition/recognizers/high-res/android/tf-model.android.ts
@@ -1,38 +1,29 @@
+import { promisify } from "../../utils.android";
 import FirebaseCustomRemoteModel = com.google.firebase.ml.custom.FirebaseCustomRemoteModel;
 import FirebaseModelDownloadConditions = com.google.firebase.ml.common.modeldownload.FirebaseModelDownloadConditions;
 import FirebaseModelManager = com.google.firebase.ml.common.modeldownload.FirebaseModelManager;
-import OnSuccessListener = com.google.android.gms.tasks.OnSuccessListener;
-import OnFailureListener = com.google.android.gms.tasks.OnFailureListener;
 
 const MODEL_NAME = "model";
 let _customModel: FirebaseCustomRemoteModel;
-export async function getAndroidCustomModel() {
+export async function getCustomModel(): Promise<FirebaseCustomRemoteModel> {
     if (!_customModel) {
         _customModel = new FirebaseCustomRemoteModel.Builder(
             MODEL_NAME
         ).build();
+
+        const firebaseModelManager = FirebaseModelManager.getInstance();
+        const isDownloaded = firebaseModelManager.isModelDownloaded(_customModel);
+
+        await promisify(isDownloaded);
+
         const conditions = new FirebaseModelDownloadConditions.Builder().build();
+        const downloadPromise = firebaseModelManager.download(_customModel, conditions);
 
-        const downloadPromise = new Promise((resolve, reject) => {
-            FirebaseModelManager.getInstance()
-                .download(_customModel, conditions)
-                .addOnSuccessListener(
-                    new OnSuccessListener({
-                        onSuccess() {
-                            resolve();
-                        },
-                    })
-                )
-                .addOnFailureListener(
-                    new OnFailureListener({
-                        onFailure(ex) {
-                            reject(ex);
-                        },
-                    })
-                );
-        });
-
-        await downloadPromise;
+        await promisify(downloadPromise);
     }
     return _customModel;
+}
+
+export function isCustomModelReady(): boolean {
+    return !!_customModel;
 }
--- src/internal/activity-recognition/recognizers/high-res/index.ts
+++ src/internal/activity-recognition/recognizers/high-res/index.ts
@@ -2,21 +2,12 @@ import { android as androidApp } from "tns-core-modules/application/application"
 
 import { ActivityRecognizer } from "..";
 import { getAndroidHighResRecognizer } from "./android/recognizer.android";
-import { getAndroidCustomModel } from "./android/tf-model.android";
 
 import FirebaseCustomRemoteModel = com.google.firebase.ml.custom.FirebaseCustomRemoteModel;
 
 export function getHighResRecognizer(): ActivityRecognizer {
     if (androidApp) {
         return getAndroidHighResRecognizer();
-    } else {
-        return null;
-    }
-}
-
-export async function getRecognizerModel(): Promise<FirebaseCustomRemoteModel> {
-    if (androidApp) {
-        return await getAndroidCustomModel();
     } else {
         return null;
     }
