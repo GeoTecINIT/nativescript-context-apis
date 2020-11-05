cfab782e3d9b82187b3e9ac7852e766249222c06
Miguel <al341802@uji.es>
2020-11-05 16:28:00 +0100

    Replace tns-platform-declarations by @nativescript/types

--- src/references.d.ts
+++ src/references.d.ts
@@ -1,5 +1,5 @@
-/// <reference path="./node_modules/tns-platform-declarations/android-29.d.ts" />
-/// <reference path="./node_modules/tns-platform-declarations/ios.d.ts" />
+/// <reference path="./node_modules/@nativescript/types-android/lib/android-29.d.ts" />
+/// <reference path="./node_modules/@nativescript/types-ios/index.d.ts" />
 
 /// <reference path="./typings/android/native-lib.d.ts" />
 /// <reference path="./typings/android/play-services-location.d.ts" />
