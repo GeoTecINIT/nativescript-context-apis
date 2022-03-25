057ba2f4a4bcd653b20d4e80e46d2d24195aa3b7
Alberto González <10727467+agonper@users.noreply.github.com>
2022-03-25 16:54:01 +0000

    ci: fix cmdline-tools path

--- ci-tools/wait-for-emulator.sh
+++ ci-tools/wait-for-emulator.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
 
-ls $ANDROID_HOME/cmdline-tools
-
 # Install AVD files
-echo "y" | $ANDROID_HOME/cmdline-tools/bin/sdkmanager --install 'system-images;android-27;google_apis;x86'
+echo "y" | $ANDROID_HOME/cmdline-tools/latest/bin/sdkmanager --install 'system-images;android-27;google_apis;x86'
 
 # Create emulator
-echo "no" | $ANDROID_HOME/cmdline-tools/bin/avdmanager create avd -n xamarin_android_emulator -k 'system-images;android-27;google_apis;x86' --force
+echo "no" | $ANDROID_HOME/cmdline-tools/latest/bin/avdmanager create avd -n xamarin_android_emulator -k 'system-images;android-27;google_apis;x86' --force
 
 $ANDROID_HOME/emulator/emulator -list-avds
 
