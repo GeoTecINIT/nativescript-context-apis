765359c6d0332c9f5d5d0e0fb7e5ac3b27bc37ed
Alberto González <10727467+agonper@users.noreply.github.com>
2022-03-28 16:04:17 +0100

    chore: run NS8 migration

--- demo/nativescript.config.ts
+++ demo/nativescript.config.ts
@@ -2,16 +2,10 @@ import { NativeScriptConfig } from '@nativescript/core';
 
 export default {
     id: 'org.nativescript.demo',
-    main: './app/app.js',
+    appPath: 'app',
     appResourcesPath: './app/App_Resources',
-    webpackConfigPath: 'webpack.config.js',
-    ios: {
-        discardUncaughtJsExceptions: true
-    },
     android: {
-        discardUncaughtJsExceptions: true,
         v8Flags: '--nolazy --expose_gc',
-        "markingMode": "none",
-        "suppressCallJSMethodExceptions": false
+        markingMode: 'none'
     }
 } as NativeScriptConfig;
