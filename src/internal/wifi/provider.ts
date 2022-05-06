4295ec694d9d4711d2e265add4b6d2caf433dd21
Alberto González <10727467+agonper@users.noreply.github.com>
2022-05-06 18:46:19 +0100

    feat: add wifi scan provider

---
+++ src/internal/wifi/provider.ts
@@ -0,0 +1,149 @@
+import { WifiScanAdapter } from "./common";
+import { WifiFingerprint } from "./fingerprint";
+import { Observable } from "rxjs";
+
+const MIN_SCAN_INTERVAL = 5000;
+
+export class WifiScanProvider {
+  constructor(private adapter: WifiScanAdapter) {}
+
+  isReady(): Promise<boolean> {
+    return this.adapter.isReady();
+  }
+
+  prepare(): Promise<void> {
+    return this.adapter.prepare();
+  }
+
+  async acquireWifiFingerprint(ensureIsNew = true): Promise<WifiFingerprint> {
+    return this.adapter.acquireWifiFingerprint(ensureIsNew);
+  }
+
+  wifiFingerprintStream(
+    options: StreamOptions = {}
+  ): Observable<WifiFingerprint> {
+    const safeOptions = fixStreamOptions(options);
+    if (safeOptions.ensureAlwaysNew) {
+      return this.wifiFingerprintStreamWithoutDuplicates(
+        safeOptions.grouping,
+        safeOptions.continueOnFailure
+      );
+    } else {
+      return this.wifiFingerprintStreamWithDuplicates(
+        safeOptions.interval,
+        safeOptions.continueOnFailure
+      );
+    }
+  }
+
+  private wifiFingerprintStreamWithoutDuplicates(
+    grouping: FingerprintGrouping,
+    continueOnFailure: boolean
+  ): Observable<WifiFingerprint> {
+    return new Observable<WifiFingerprint>((subscriber) => {
+      let unsubscribed = false;
+      const performScans = async () => {
+        try {
+          for (let i = 0; i < grouping; i++) {
+            const result = await this.adapter.acquireWifiFingerprint(true);
+            subscriber.next(result);
+            await new Promise((resolve) =>
+              setTimeout(resolve, MIN_SCAN_INTERVAL)
+            );
+            if (unsubscribed) break;
+          }
+        } catch (err) {
+          if (continueOnFailure) {
+            console.error("WifiScanProvider ERROR:", err);
+          } else {
+            subscriber.error(err);
+          }
+        }
+      };
+
+      performScans();
+      const intervalId = setInterval(() => {
+        performScans();
+      }, getIntervalForGrouping(grouping));
+
+      return () => {
+        unsubscribed = true;
+        clearInterval(intervalId);
+      };
+    });
+  }
+
+  private wifiFingerprintStreamWithDuplicates(
+    interval: number,
+    continueOnFailure: boolean
+  ): Observable<WifiFingerprint> {
+    return new Observable<WifiFingerprint>((subscriber) => {
+      const performScan = async () => {
+        try {
+          const result = await this.adapter.acquireWifiFingerprint(false);
+          subscriber.next(result);
+        } catch (err) {
+          if (continueOnFailure) {
+            console.error("WifiScanProvider ERROR:", err);
+          } else {
+            subscriber.error(err);
+          }
+        }
+      };
+
+      performScan();
+      const intervalId = setInterval(() => {
+        performScan();
+      }, interval);
+
+      return () => {
+        clearInterval(intervalId);
+      };
+    });
+  }
+}
+
+function fixStreamOptions(options: StreamOptions): StreamOptions {
+  const copy = { ...options };
+  if (copy.ensureAlwaysNew === undefined) {
+    copy.ensureAlwaysNew = true;
+  }
+  if (copy.grouping === undefined || !copy.ensureAlwaysNew) {
+    copy.grouping = FingerprintGrouping.NONE;
+  }
+  if (
+    (copy.interval === undefined || copy.interval < MIN_SCAN_INTERVAL) &&
+    !copy.ensureAlwaysNew
+  ) {
+    copy.interval = MIN_SCAN_INTERVAL;
+  }
+  if (copy.continueOnFailure !== undefined) {
+    copy.continueOnFailure = false;
+  }
+
+  return copy;
+}
+
+export interface StreamOptions {
+  ensureAlwaysNew?: boolean;
+  grouping?: FingerprintGrouping;
+  interval?: number;
+  continueOnFailure?: boolean;
+}
+
+export enum FingerprintGrouping {
+  NONE = 1,
+  INTERMEDIATE = 2,
+  MAX = 4,
+}
+
+function getIntervalForGrouping(grouping: FingerprintGrouping): number {
+  switch (grouping) {
+    case FingerprintGrouping.NONE:
+      return 30 * 1000; // 30s
+    case FingerprintGrouping.INTERMEDIATE:
+      return 60 * 1000; // 60s
+    case FingerprintGrouping.MAX:
+      return 2 * 60 * 1000; // 2 min
+  }
+}
