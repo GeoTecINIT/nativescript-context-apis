2e2ab2b11e955f16b4e4900cb10ae294a509bf1c
Alberto González <10727467+agonper@users.noreply.github.com>
2022-05-06 18:46:09 +0100

    feat: add wifi scan android adapter

---
+++ src/internal/wifi/android/adapter.ts
@@ -0,0 +1,88 @@
+import { WifiScanAdapter } from "../common";
+import { WifiFingerprint } from "../fingerprint";
+import { enableLocationRequest, isEnabled } from "@nativescript/geolocation";
+import { Utils } from "@nativescript/core";
+import { getSeenWifiApsFrom } from "./parsers";
+
+export class AndroidWifiScanAdapter implements WifiScanAdapter {
+  constructor(
+    private wifiManager: android.net.wifi.WifiManager = Utils.android
+      .getApplicationContext()
+      .getSystemService(android.content.Context.WIFI_SERVICE)
+  ) {}
+
+  isReady(): Promise<boolean> {
+    return isEnabled();
+  }
+
+  prepare(): Promise<void> {
+    return enableLocationRequest(false, true);
+  }
+
+  async acquireWifiFingerprint(ensureIsNew = true): Promise<WifiFingerprint> {
+    const ready = await this.isReady();
+    if (!ready) {
+      throw new Error(
+        "Could not acquire wifi fingerprint. Insufficient permissions or disabled location services. Please, call prepare() first before requesting a fingerprint."
+      );
+    }
+
+    try {
+      await this.setupScanReceiver();
+      return this.getLastFingerprint();
+    } catch (err) {
+      if (ensureIsNew) throw err;
+      console.error("WifiScanAdapter error:", err);
+      return this.getLastFingerprint();
+    }
+  }
+
+  private setupScanReceiver(): Promise<void> {
+    return new Promise<void>((resolve, reject) => {
+      const scanReceiver = new es.uji.geotec.contextapis.wifi.WifiScanReceiver(
+        new es.uji.geotec.contextapis.wifi.WifiScanReceiverDelegate({
+          onReceive(
+            context: android.content.Context,
+            intent: android.content.Intent
+          ) {
+            Utils.android
+              .getApplicationContext()
+              .unregisterReceiver(scanReceiver);
+            const success = intent.getBooleanExtra(
+              android.net.wifi.WifiManager.EXTRA_RESULTS_UPDATED,
+              false
+            );
+            if (success) {
+              resolve();
+            } else {
+              reject(new Error("Results not updated"));
+            }
+          },
+        })
+      );
+      const intentFilter = new android.content.IntentFilter();
+      intentFilter.addAction(
+        android.net.wifi.WifiManager.SCAN_RESULTS_AVAILABLE_ACTION
+      );
+      Utils.android
+        .getApplicationContext()
+        .registerReceiver(scanReceiver, intentFilter);
+      const success = this.wifiManager.startScan();
+      if (!success) {
+        reject(
+          new Error(
+            "Could not start scan! Reason: too many requests, device idle or unlikely hardware failure"
+          )
+        );
+      }
+    });
+  }
+
+  private getLastFingerprint(): WifiFingerprint {
+    const results = this.wifiManager.getScanResults();
+    return {
+      seen: getSeenWifiApsFrom(results),
+      timestamp: new Date(),
+    };
+  }
+}
