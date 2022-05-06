60a58f69fc1abc0ee52400e1868f99f5d52e33aa
Alberto González <10727467+agonper@users.noreply.github.com>
2022-05-06 18:45:48 +0100

    feat: add wifi fingerprint model

---
+++ src/internal/wifi/fingerprint.ts
@@ -0,0 +1,43 @@
+export interface WifiFingerprint {
+  seen: Array<WifiApInfo>;
+  timestamp: Date;
+}
+
+export interface WifiApInfo {
+  ssid: string;
+  bssid: string;
+  capabilities: string;
+  frequency: number;
+  centerFreq0: number;
+  centerFreq1: number;
+  bandwidth: ChannelBandwidth;
+  standard: WifiStandard;
+  ageMicro: number;
+  chainsInfo: Array<WifiChainInfo>;
+  rssi: number;
+}
+
+export enum ChannelBandwidth {
+  UNSPECIFIED = "?",
+  MHZ20 = "MHZ20",
+  MHZ40 = "MHZ40",
+  MHZ80 = "MHZ80",
+  MHZ160 = "MHZ160",
+  MHZ80_80 = "MHZ80+80",
+  MHZ320 = "MHZ320",
+}
+
+export enum WifiStandard {
+  UNKNOWN = "unknown",
+  LEGACY = "11a/b/g",
+  N = "11n",
+  AC = "11ac",
+  AX = "11ax",
+  AD = "11ad",
+  BE = "11be",
+}
+
+export interface WifiChainInfo {
+  id: number;
+  rssi: number;
+}
